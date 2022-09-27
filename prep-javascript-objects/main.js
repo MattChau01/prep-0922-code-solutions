ffde269641e664de4d49f8590dac075bd831c1b3
Matthew Chau <matthewwchau@gmail.com>
2022-09-27 04:04:05 +0000

    completed prep-javascript-objects exercise.

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,16 @@
+var person = {
+  firstName: 'Matthew',
+  lastName: 'Chau',
+  hobby: 'Kayaking',
+  currentJob: 'QA Lead',
+  previousJob: 'Logistics Coordinator'
+};
+console.log(person);
+
+const fullName = person.firstName + ' ' + person.lastName;
+console.log('The person\'s name is: ', fullName);
+const currentJob = person.currentJob;
+console.log('The person\'s current jobs is:', currentJob);
+const previousJob = person.previousJob;
+console.log('The person\'s previous job is:', previousJob);
+console.log(person);
